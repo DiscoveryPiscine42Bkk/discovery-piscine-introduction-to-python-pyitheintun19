9045eb9268bb08e379194a682db8c00b5b390547
Pyi Thein Tun <167497580+pyitheintun19@users.noreply.github.com>
2025-06-25 19:10:39 +0000

    add play_with_arrays.py v2

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,7 +1,9 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = []
+
 for new in range(len(original_array)):
-    new_array.append(original_array[new]+2)
+    if original_array[new] >=5:
+        new_array.append(original_array[new]+2)
 
 print(f"Original array: {original_array}")
 print(f"New array: {new_array}")
