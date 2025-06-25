0eb45189e37d90edddf7ffb0d52bda46828508a1
Pyi Thein Tun <167497580+pyitheintun19@users.noreply.github.com>
2025-06-25 15:31:54 +0000

    add calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 + num2}")
+
+if num2 == 0:
+    print("Undefined!")
+else:
+    print(f"{num1} / {num2} = {num1 + num2}")
+
+print(f"{num1} * {num2} = {num1 + num2}")
