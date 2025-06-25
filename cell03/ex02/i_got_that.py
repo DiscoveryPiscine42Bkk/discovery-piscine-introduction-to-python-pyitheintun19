2fb1d5d8ab99955bba4d9284fd72660e208c1821
Pyi Thein Tun <167497580+pyitheintun19@users.noreply.github.com>
2025-06-25 12:08:11 +0000

    add i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+user_input = input("What you gotta say? : ")
+while True:
+        
+        if user_input == "STOP":
+            break
+            
+        print("I got that! Anything else? : ", end="")
+        user_input = input()
