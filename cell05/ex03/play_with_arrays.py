afdb5349b675c80eaccfffd984246186a5e8967a
Pyi Thein Tun <167497580+pyitheintun19@users.noreply.github.com>
2025-06-25 19:45:11 +0000

    update arrays v3

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,8 +1,9 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = []
+new_array = set()
 
 for new in range(len(original_array)):
-    new_array.extend([original_array[new] + 2])
+    if original_array[new] >= 5:
+        new_array.add(original_array[new] + 2)
 
 print(f"Original array: {original_array}")
 print(f"New array: {new_array}")
